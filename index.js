b1eed23ce60db95d5b6430d26e8f1ca4161c1c34
bennyman123abc <bennyman123abc@hotmail.com>
2019-02-28 22:58:53 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+/*eslint-disable no-unused-params, no-shadow-global*/
+var express = require("express");
+var app = require("express")(); // create new express app instance
+var server = require("http").createServer(app); // create http server instance
+var io = require("socket.io").listen(server); // create socket.io server listening to the http server
+
+var fs = require("fs");
+
+const port = 9000; // define the port the application will use here
+
+server.listen(port, function() {
+   console.log(`Listening on port ${port}`); 
+});
+
+app.get("/", function(_, res) {
+    res.sendFile(__dirname + "/src/index.html");
+});
+
+io.on("connection", function(socket) {
+    console.log(`New socket connection with ID ${socket.id}`);
+    socket.emit("connMessage");
+    socket.on("startGame", function(name) {
+        var q = 0;
+        console.log("Starting game...");
+        var game = loadGame(name);
+        console.log(game);
+        socket.emit("loadedGame");
+        console.log(game.questions[q]);
+        q += 1;
+    });
+});
+
+function loadGame(name) {
+    return JSON.parse(fs.readFileSync(__dirname + `/games/${name}.json`));
+}
